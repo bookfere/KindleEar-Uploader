7b30bc3172b682081963fdc5c1ce946a7c13caad
kindlefere <kindlefere@gmail.com>
2016-11-14 22:22:49 +0800

    2016年11月14日 22时22分49秒

---
+++ README.md
@@ -0,0 +1,17 @@
+# KindleEar上传脚本
+
+打开 Google 云端 Shell，全选复制以下命令，按照提示输入 Gmail 和 APPID 即可轻松上传。
+
+```
+wget https://raw.githubusercontent.com/kindlefere/KindleEar-Uploader/master/uploader.sh && \
+chmod +x uploader.sh && \
+./uploader.sh
+```
+
+KindleEar 上传教程：https://kindlefere.com/post/19.html  
+KindleEar 项目地址：https://github.com/cdhigh/KindleEar
+
+---
+
+Kindle 伴侣 - 为静心阅读而生  
+http://kindlefere.com
---
+++ uploader.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# ------------------------------------------
+# 名称：KindleEar安装脚本
+# 作者：kindlefere.com
+# 页面：http://kindlefere.com/post/19.html
+# ------------------------------------------
+
+if [ ! -d "./KindleEar" ]
+then
+    git clone https://github.com/cdhigh/KindleEar.git
+fi
+
+cd KindleEar
+
+cemail=$(sed -n "s/^SRC_EMAIL = \"\(.*\)\".*#.*/\1/p" ./config.py)
+cappid=$(sed -n "s/^DOMAIN = \"https:\/\/\(.*\)\.appspot.com\".*#.*/\1/p" ./config.py)
+response='y'
+
+echo '当前的 Gmail 为：'$cemail
+echo '当前的 APPID 为：'$cappid
+
+if [ ! $cemail = "akindleear@gmail.com" -o ! $cappid = "kindleear" ]
+then
+    read -r -p "是否修改 APP 信息? [y/N] " response
+fi
+
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    echo -n "请输入你的 Gmail 地址："
+    read email
+    echo "您输入的 Gmail 地址是：'$email'"
+    sed -i "s/^SRC_EMAIL = \".*\"/SRC_EMAIL = \"$email\"/g" ./config.py
+    echo -n "请输入你的 APP ID："
+    read appid
+    echo "您输入的 Gmail 地址是：'$appid'"
+    sed -i "s/^application: .*/application: $appid/g" ./app.yaml ./module-worker.yaml
+    sed -i "s/^DOMAIN = \"https:\/\/.*\.appspot.com\"/DOMAIN = \"https:\/\/$appid\.appspot.com\"/g" ./config.py
+fi
+
+appcfg.py update app.yaml module-worker.yaml --no_cookie --noauth_local_webserver
+appcfg.py update . --no_cookie --noauth_local_webserver
